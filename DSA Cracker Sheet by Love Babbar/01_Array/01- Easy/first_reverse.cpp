7e3d74e406c9fd2005f008a60a5598cc76de5479
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-04-16 23:02:59 +0530

    01 - Reverse an array

---
+++ DSA Cracker Sheet by Love Babbar/01_Array/01- Easy/first_reverse.cpp
@@ -0,0 +1,49 @@
+/*
+--------------------------------------------
+Video reference
+   - Love B : Lecture 09
+   -  Ayushi Sharma : https://youtu.be/O2qhSwSf8HQ
+   
+Article reference
+   - https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+-------------------------------------------
+*/
+
+
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n) {
+
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n) {
+    
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    int arr[6] = {1,4,0,5,-2,15};
+    int brr[5] = {2,6,3,9,4};
+
+    reverse(arr, 6);
+    reverse(brr, 5);
+
+    printArray(arr, 6);
+    printArray(brr, 5);
+
+
+    return 0;
+}
