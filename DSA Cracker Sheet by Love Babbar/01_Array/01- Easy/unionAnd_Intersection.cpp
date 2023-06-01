11d262672b60cd72dec5feeba22c9f415593ff09
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-06-01 22:21:10 +0530

    Create unionAnd_Intersection.cpp

---
+++ DSA Cracker Sheet by Love Babbar/01_Array/01- Easy/unionAnd_Intersection.cpp
@@ -0,0 +1,47 @@
+/*
+--------------------------------
+01- Union of two arrays
+
+ Using, 
+  - Set
+  - unordered map
+--------------------------------
+*/
+
+
+class Solution {
+    public:
+  
+    int doUnion(int a[], int n, int b[], int m) {
+
+        set<int>st;
+        
+        for(int i=0; i<n; i++) {
+            st.insert(a[i]);
+        }
+        for(int j=0; j<m; j++) {
+            st.insert(b[j]);
+        }
+        return st.size();
+    }
+};
+
+
+
+class Solution {
+    public:
+
+    int doUnion(int a[], int n, int b[], int m) {
+        
+       unordered_map<int, int> Unionans;
+         
+        for(int i=0; i<n; i++) {
+            Unionans[a[i]]++;
+        }
+        
+        for(int i=0; i<m; i++){
+            Unionans[b[i]]++;
+        }
+        return Unionans.size();
+    }
+};
