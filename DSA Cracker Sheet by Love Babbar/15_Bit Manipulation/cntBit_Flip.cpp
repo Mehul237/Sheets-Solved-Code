27edd5c8e112efbe39b889e98b6de6406ceade9e
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-12-04 14:23:09 +0530

    02 - Count number of bits to be flipped to convert A to B
    - Perform first Xor after if it is Odd then cnt++ and move the next bit by right shift (A^B >> 1)

---
+++ DSA Cracker Sheet by Love Babbar/15_Bit Manipulation/cntBit_Flip.cpp
@@ -0,0 +1,45 @@
+
+/*
+------------------------------------------------------------------------
+Ex: 
+
+Input:  A = 10, B = 20
+Output: 4
+Explanation:
+
+  A  ->  0 1 0 1 0
+  B  ->  1 0 1 0 0 
+--------------------
+A ^ B :  1 1 1 1 0   (Convert A to B)
+
+
+
+ Step-01: Xor A and B
+
+ Step-02: After Xor cnt the number of odd and cnt++ in that case and 
+          right shift the next bit
+ 
+----------------------------------------------------------------------
+*/
+
+
+class Solution {
+    public:
+
+    int countBitsFlip(int a, int b) {
+        
+        // Step-01
+        int xorAandB = a ^ b;
+        int cnt = 0;
+        
+        while(xorAandB) {
+           
+           // Step-02
+           if(xorAandB & 1) // If it is odd then, cnt++
+              cnt++;
+              xorAandB >>= 1;
+        }
+        return cnt++;
+        
+    }
+};
